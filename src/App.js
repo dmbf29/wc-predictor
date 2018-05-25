894ddf64b3e33935f54b6c5e0bd9219f75c4e0e9
Douglas Berkley <douglasmberkley@gmail.com>
2018-05-25 11:05:36 +0900

    added matches container

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import banner from './banner.gif';
 import './App.css';
+import MatchesContainer from './components/MatchesContainer'
 
 class App extends Component {
   render() {
@@ -10,6 +11,7 @@ class App extends Component {
           <img src={banner} className="App-banner" alt="banner" />
           <h1 className="App-title"></h1>
         </header>
+        <MatchesContainer />
       </div>
     );
   }
---
+++ src/components/MatchesContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class MatchesContainer extends Component {
+  render() {
+    return (
+      <div>
+        Matches
+      </div>
+    )
+  }
+}
+
+export default MatchesContainer
