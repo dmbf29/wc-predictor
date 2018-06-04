3e7b66ea500db04eecf32cd9054091b2d8691b94
Douglas Berkley <douglasmberkley@gmail.com>
2018-06-04 20:05:33 +0900

    added sign in sign up nav bar links if not signed in

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -26,18 +26,38 @@ class Navbar extends Component {
         <img src={icon} className="navbar-wagon-brand" alt="banner" />
         <div className="navbar-wagon-right">
         <div className="subnav-wagon">
-          <Link to='/' className="subnav-wagon-item subnav-wagon-link subnav-spacing subnav-border-right">PREDICTIONS</Link>
-          <Link to='/leagues' className="subnav-wagon-item subnav-wagon-link subnav-spacing subnav-border-right">LEAGUES</Link>
+          { localStorage.jwt === undefined &&
+            <div>
+              <Link to='/sign_in' onClick={this.toggleDrop} className="subnav-wagon-item subnav-wagon-link subnav-spacing subnav-border-right">SIGN IN</Link>
+              <Link to='/sign_up' onClick={this.toggleDrop} className="subnav-wagon-item subnav-wagon-link subnav-spacing subnav-border-right">SIGN UP</Link>
+            </div>
+          }
+          { localStorage.jwt !== undefined &&
+            <div>
+              <Link to='/' className="subnav-wagon-item subnav-wagon-link subnav-spacing subnav-border-right">PREDICTIONS</Link>
+              <Link to='/leagues' className="subnav-wagon-item subnav-wagon-link subnav-spacing subnav-border-right">LEAGUES</Link>
+            </div>
+          }
         </div>
 
         <div className="hidden-xs">
           <div className="dropdown">
             <img onClick={this.toggleDrop} src={avatar} className="avatar dropdown-toggle" alt="avatar" id="navbar-wagon-menu" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" />
             <ul className="dropdown-menu dropdown-menu-right navbar-wagon-dropdown-menu">
-              <li><Link onClick={this.toggleDrop} to='/account' className="dropdown-item">Account</Link></li>
-              <li><Link onClick={this.toggleDrop} to='/league_join' className="dropdown-item">Join League</Link></li>
-              <li><Link onClick={this.toggleDrop} to='/league_create' className="dropdown-item">Create League</Link></li>
-              <li><Link to='/' onClick={this.signOut} className="dropdown-item">Sign Out</Link></li>
+              { localStorage.jwt === undefined &&
+                <div>
+                  <li><Link to='/sign_in' onClick={this.toggleDrop} className="dropdown-item">Sign In</Link></li>
+                  <li><Link to='/sign_up' onClick={this.toggleDrop} className="dropdown-item">Sign Up</Link></li>
+                </div>
+              }
+              { localStorage.jwt !== undefined &&
+                <div>
+                  <li><Link onClick={this.toggleDrop} to='/account' className="dropdown-item">Account</Link></li>
+                  <li><Link onClick={this.toggleDrop} to='/league_join' className="dropdown-item">Join League</Link></li>
+                  <li><Link onClick={this.toggleDrop} to='/league_create' className="dropdown-item">Create League</Link></li>
+                  <li><Link to='/' onClick={this.signOut} className="dropdown-item">Sign Out</Link></li>
+                </div>
+              }
             </ul>
           </div>
         </div>
