669f0824427164dcf31209a8fac4369328144b78
Douglas Berkley <douglasmberkley@gmail.com>
2018-06-05 14:39:21 +0900

    added favicon

--- src/index.js
+++ src/index.js
@@ -5,11 +5,15 @@ import 'bootstrap/dist/css/bootstrap.css'
 import App from './components/App';
 import { BrowserRouter } from 'react-router-dom'
 import registerServiceWorker from './registerServiceWorker';
+import Favicon from 'react-favicon';
 
 ReactDOM.render((
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <div>
+    <Favicon src="https://raw.githubusercontent.com/dmbf29/wc-predictor/master/public/favicon.ico" />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </div>
 ), document.getElementById('root'));
 
 registerServiceWorker();
